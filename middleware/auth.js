1f6e94b5afcce359eb8d310f6629ab1e760a6db3
Saqibi4213 <mehriamosawer@gmail.com>
2024-11-04 18:43:34 +0430

    error solving

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,17 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const authenticateJWT = (req, res, next) => {
-  const token = req.header("Authorization");
+  const token = req.header('Authorization');
   if (!token) {
-    return res.status(401).json({ message: "Access denied, no token provided" });
+    return res.status(401).json({ message: 'Access denied, no token provided' });
   }
 
   try {
-    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET); // Extract token
+    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
     req.user = decoded;
-    next();
+    return next(); // Added return statement to match the consistent-return rule
   } catch (error) {
-    res.status(400).json({ message: "Invalid token" });
+    return res.status(400).json({ message: 'Invalid token' });
   }
 };
 
